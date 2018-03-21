fde0d0f63e60789511044f6a7dd616cb6dea3250
Michiel Knevels <michiel.knevels@mobitrace.eu>
2018-03-21 13:24:07 +0100

    M4-1211: revert

--- react-native-highcharts.js
+++ react-native-highcharts.js
@@ -33,14 +33,14 @@ class ChartWeb extends Component {
             extraData: `<script> window.extraData = `,
             mid: `        </script>
                         
-                        <script>${this.props.jquery}</script>
-                        ${this.props.stock ? '<script>' + this.props.highstock + '</script>'
-                : '<script>' + this.props.highcharts + '</script>'}
-                        ${this.props.more ? '<script>' + this.props.highchartsMore + '</script>'
+                        <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+                        ${this.props.stock ? '<script src="https://code.highcharts.com/stock/highstock.js"></script>'
+                : '<script src="https://code.highcharts.com/highcharts.js"></script>'}
+                        ${this.props.more ? '<script src="https://code.highcharts.com/highcharts-more.js"></script>'
                 : ''}
-                        ${this.props.guage ? '<script>' + this.props.solidGauge +'</script>'
+                        ${this.props.guage ? '<script src="https://code.highcharts.com/modules/solid-gauge.js"></script>'
                 : ''}
-                        <script>${this.props.exporting}</script>
+                        <script src="https://code.highcharts.com/modules/exporting.js"></script>
                         <script>
                         $(function () {
                             Highcharts.setOptions(${JSON.stringify(this.props.options)});
