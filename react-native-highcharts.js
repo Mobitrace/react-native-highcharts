88557143701659046860a1ccd4adfe1992426b14
michielkn-mobitrace <35763788+michielkn-mobitrace@users.noreply.github.com>
2019-09-09 11:38:29 +0200

    Update react-native-highcharts.js

--- react-native-highcharts.js
+++ react-native-highcharts.js
@@ -4,10 +4,10 @@ import {
     StyleSheet,
     Text,
     View,
-    WebView,
     Image,
     Dimensions
 } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 const win = Dimensions.get('window');
 class ChartWeb extends Component {
@@ -64,10 +64,13 @@ class ChartWeb extends Component {
 
     // used to resize on orientation of display
     reRenderWebView(e) {
+        /*
         this.setState({
             height: e.nativeEvent.layout.height,
             width: e.nativeEvent.layout.width,
         })
+         */
+
     }
 
     render() {
